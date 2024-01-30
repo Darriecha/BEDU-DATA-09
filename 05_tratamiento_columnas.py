5890d55cbd7666069e522c502d316f66bd6aeef6
Darriecha <148508782+Darriecha@users.noreply.github.com>
2024-01-29 20:21:44 -0600

    pandas examples

---
+++ 05_tratamiento_columnas.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+ENCODING = 'ISO-8859-1'
+dataframe = pd.read_csv('datasets\car_crashes.csv', encoding=ENCODING)
+
+
+print(dataframe.head(1))
+
+dataframe = dataframe.rename(columns={'Weekend?':'fin_de_semana', 'Year':'año'})
+
+print(dataframe.head(1))
