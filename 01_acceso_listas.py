87b89863ce3405f8c899645bba839e42c4928fff
Darriecha <148508782+Darriecha@users.noreply.github.com>
2024-01-29 19:29:17 -0600

    add requirements

---
+++ 01_acceso_listas.py
@@ -0,0 +1,13 @@
+#Listas y acceso a ellas se puede hacer por un corchete o con la palabra list
+frutas = list()
+
+print(frutas)
+print(type(frutas))
+
+frutas.append('manzana')
+frutas.append('plátano')
+frutas.append('sandía')
+frutas.append('fresas')
+frutas.append('cocos')
+print(type(frutas))
+print(frutas)
