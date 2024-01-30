9ce1391c9c89b093e16f36d30dbaa16d110a233a
Darriecha <148508782+Darriecha@users.noreply.github.com>
2024-01-29 21:18:58 -0600

    more samples

---
+++ 09_dataframe_to_csv.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+#Objeto a convertir
+dataframe = pd.DataFrame({
+    'Columna1': [1, 2, 3],
+    'Columna2': ['A', 'B', 'C'],
+    'Columna3': [True, False, True]
+})
+
+filename = 'datasets/new_csv_from_pd.csv'
+
+print(dataframe.head())
+
+dataframe.to_csv(filename, index=False)
+
+print('XD')
+
+
